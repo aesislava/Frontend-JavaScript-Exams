14c81bcee7da01ef419458816d18e6914fa7bc17
aesislava <aesislava@gmail.com>
2014-07-19 17:10:57 +0300

    ...

---
+++ 02/03Canvas-Triangles/app.js
@@ -0,0 +1,7 @@
+$("document").ready(function() {
+	var canvas = document.getElementById('triangles');
+    var context = canvas.getContext("2d");
+    var cW = canvas.width;
+    var cH = canvas.height;
+	alert(cH + ' ' + cW);
+});
