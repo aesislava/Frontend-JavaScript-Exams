284d0fb03cb7ca650c811fad546d5ad369521275
aesislava <aesislava@gmail.com>
2014-07-19 16:35:11 +0300

    ...

--- 02/01CorruptedData/app.js
+++ 02/01CorruptedData/app.js
@@ -1,2 +1,19 @@
-var data = require("data");
-console.log(data);
+"use strict";
+
+var data = require("./data"),
+    i,
+    j,
+    dublicates = [],
+    len = data.length;
+
+for (i = 0; i < len; i++) {
+    for (j = i + 1; j < len; j++) {
+        if (data[i].fields.date === data[j].fields.date && data[i].fields.student === data[j].fields.student) {
+            dublicates.push(data[j]);
+        }
+    }
+}
+
+console.log(dublicates.sort(function(a, b) {
+    return a.pk - b.pk;
+}));
